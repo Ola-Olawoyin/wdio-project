85e369d80f33e26e6c0dd93da1e274a395b790c3
Ola-Olawoyin <oola.olawoyin@gmail.com>
2024-12-30 22:06:41 +0000

    Add Input Handling Test

---
+++ test/features/demo/webInteration.feature
@@ -0,0 +1,11 @@
+Feature: Web Interaction
+
+    @demo
+    Scenario Outline: Run first demo feature
+        Given A Web page is opened
+        When I perform web interactions
+
+
+        Examples:
+            | TestID    |
+            | WEB_TC001 |
---
+++ test/features/step-definitions/webInteraction.ts
@@ -0,0 +1,37 @@
+import { Given, When } from '@wdio/cucumber-framework';
+
+Given(/^A Web page is opened$/, async () => {
+  await browser.url('/inputs');
+  await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
+  await browser.maximizeWindow();
+});
+
+When(/^I perform web interactions$/, async () => {
+  /*
+   * 1. Input box
+   * Actions:
+   * 1. Type into input box
+   * 2. Clear the field and type or just add value
+   * 3. Click and type
+   * 4 Slow Typing
+   *
+   */
+  let num = 12345;
+  let strgNum = num.toString();
+  let ele = await $(`[type=number]`);
+
+  await ele.click();
+  //await ele.scrollIntoView()
+  //await ele.moveTo()
+  // await ele.setValue(12345);
+
+  /*
+  Handling slow typing
+  */
+  await browser.pause(5000);
+  for (let i = 0; i < strgNum.length; i++) {
+    let charStr = strgNum.charAt(i);
+    await browser.pause(1000);
+    await browser.keys(charStr);
+  }
+});
